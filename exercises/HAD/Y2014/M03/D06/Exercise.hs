cf2232e0d3d69e18693178722547beace3ca91f8
nadirs <nadirsampaoli@gmail.com>
2014-03-12 22:08:38 +0100

    Improve function naming slightly

--- exercises/HAD/Y2014/M03/D06/Exercise.hs
+++ exercises/HAD/Y2014/M03/D06/Exercise.hs
@@ -18,8 +18,8 @@ module HAD.Y2014.M03.D06.Exercise where
 -- []
 --
 takeStrictlyLessThan :: Integral a => a -> [a] -> [a]
-takeStrictlyLessThan = help [] where
-    help :: Integral a => [a] -> a -> [a] -> [a]
-    help past n (x:future)
-        | n - x > 0     = x : help past (n - x) future
-    help _ _ _ = []
+takeStrictlyLessThan = iter [] where
+    iter :: Integral a => [a] -> a -> [a] -> [a]
+    iter past n (x:future)
+        | n - x > 0     = x : iter past (n - x) future
+    iter _ _ _ = []
