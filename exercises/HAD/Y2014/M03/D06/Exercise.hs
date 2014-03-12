4dbf5d67e6cf5ccffc171978d6e7bded2bf8c6a8
nadirs <nadirsampaoli@gmail.com>
2014-03-12 22:06:01 +0100

    Solve 2014-03-06

--- exercises/HAD/Y2014/M03/D06/Exercise.hs
+++ exercises/HAD/Y2014/M03/D06/Exercise.hs
@@ -17,5 +17,9 @@ module HAD.Y2014.M03.D06.Exercise where
 -- >>> takeStrictlyLessThan (42::Int) $ []
 -- []
 --
--- takeStrictlyLessThan :: Choose your poison
-takeStrictlyLessThan = undefined
+takeStrictlyLessThan :: Integral a => a -> [a] -> [a]
+takeStrictlyLessThan = help [] where
+    help :: Integral a => [a] -> a -> [a] -> [a]
+    help past n (x:future)
+        | n - x > 0     = x : help past (n - x) future
+    help _ _ _ = []
